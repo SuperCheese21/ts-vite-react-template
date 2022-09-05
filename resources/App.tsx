e93842ed044e0dd84bd104d250aaa04c64d9ba2e
SuperCheese21 <ethan.shields21@gmail.com>
2022-09-04 17:25:19 -0700

    Added react-daisyui

--- resources/App.tsx
+++ resources/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from 'react-daisyui';
+
 export const App = (): JSX.Element => (
-  <>
-    <button className="btn btn-primary">Button</button>
-  </>
+  <div className="flex h-screen">
+    <div className="m-auto">
+      <div className="text-6xl text-red-600">1</div>
+      <Button color="primary">Click me!</Button>
+    </div>
+  </div>
 );
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 module.exports = {
-  content: ['index.html', 'resources/**/*.{html,ts,tsx}'],
+  content: [
+    './resources/**/*.{html,ts,tsx}',
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
+  ],
   daisyui: {
     themes: ['light', 'dark'],
   },
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'), require('@tailwindcss/typography')],
 };
